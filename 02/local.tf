8632e693b57870a44de719f9b6914eba921dd666
ne0kk <neok.kem@gmail.com>
2025-04-24 02:40:36 +0700

    Update local.tf

--- 02/local.tf
+++ 02/local.tf
@@ -9,10 +9,10 @@ locals{
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
     serial-port-enable = "1"
     user-data  = <<EOF
-    #!/bin/bash
-    cd /var/www/html
-    echo '<html><head><title>Dragon</title></head> <body><h1>Hello!</h1><img src="https://storage.yandexcloud.net/homework-s3/images.jpg"/></body></html>' > index.html
-    EOF
+#!/bin/bash
+cd /var/www/html
+echo '<html><head><title>Dragon</title></head> <body><h1>HomeWork</h1><img src="https://storage.yandexcloud.net/homework-s3/images.jpg"/></body></html>' > index.html
+EOF
     }
 }
 
