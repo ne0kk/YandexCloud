ee4ff6b38624fd106b700551d82d9787c3dc82e2
ne0kk <neok.kem@gmail.com>
2025-04-24 01:51:24 +0700

    Update count-vm-public.tf

--- 02/count-vm-public.tf
+++ 02/count-vm-public.tf
@@ -23,15 +23,7 @@ resource "yandex_compute_instance_group" "public" {
     }
   }
 
-    metadata =  {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-    serial-port-enable = "1"
-    user-data  = <<EOF
-    #!/bin/bash
-    cd /var/www/html
-    echo '<html><head><title>Dragon</title></head> <body><h1>Hello!</h1><img src="https://storage.yandexcloud.net/homework-s3/images.jpg"/></body></html>' > index.html
-    EOF
-    }
+    metadata =  local.metadata_user_data
 
   scheduling_policy { preemptible = true }
 
